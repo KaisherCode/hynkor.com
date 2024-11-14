64b98b6d5c7da3a9b547d58fd0f0ad523bff0088
Wilson Dev <hch.wilson@hynkor.com>
2024-11-14 00:03:14 -0500

    fix tailwind.config: added colors primary, secondary and accent

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				primary: "#00AA9E",
+				secondary: "#457B9D",
+				accent: "#E63946",
+			},
+		},
 	},
 	plugins: [],
 }
